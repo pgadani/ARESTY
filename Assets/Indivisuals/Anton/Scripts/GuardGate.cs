165becef984900ad7dc3664e5fd5bd49147b6484
ShikyC <xz348@scarletmail.rutgers.edu>
2017-01-03 13:12:58 -0500

    Initial

--- Assets/Indivisuals/Anton/Scripts/GuardGate.cs
+++ Assets/Indivisuals/Anton/Scripts/GuardGate.cs
@@ -5,70 +5,102 @@ using System;
 using NPC;
 
 public class GuardGate : MonoBehaviour {
-    private BehaviorAgent behaviorAgent;
-    private GameObject guard;
+	
     public GameObject guest;
     public GameObject gate;
-    private Animator anim;
+	public GameObject gateHandle;
+	public GameObject guardPosition;
 
-    // Use this for initialization
+	private BehaviorAgent behaviorAgent;
+	private GameObject guard;
+	private NPCBehavior npcBehavior;
+	private bool gateOpened = false;
+	private RaycastHit hit;
+	public GameObject selectionIndicator;
+
+	void Awake () {
+		npcBehavior = gameObject.GetComponent <NPCBehavior> ();
+		selectionIndicator = gameObject.transform.Find ("SelectionEffect").gameObject;
+	}
+
     void Start() {
         guard = gameObject;
-        anim = guard.GetComponent<Animator>();
-        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
-        BehaviorManager.Instance.Register(behaviorAgent);
-        behaviorAgent.StartBehavior();
+        behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
+        BehaviorManager.Instance.Register (behaviorAgent);
+        behaviorAgent.StartBehavior ();
     }
 
-    // Update is called once per frame
-    void FixedUpdate() {
+	protected Node OpenGate () {
+		return new Sequence (
+			new LeafAssert (() => Vector3.Distance (gate.transform.position, guest.transform.position) < 20.0f),
+			new LeafAssert (() => !gateOpened),
+			npcBehavior.NPCBehavior_Stop (),
+			new Selector (
+				new Sequence (
+					new LeafAssert (() => Vector3.Distance (gate.transform.position, gameObject.transform.position) > 40.0f),
+					npcBehavior.NPCBehavior_GoTo (gateHandle.transform, true)
+				),
+				new Sequence (
+					new LeafAssert (() => Vector3.Distance (gate.transform.position, gameObject.transform.position) <= 40.0f),
+					npcBehavior.NPCBehavior_GoTo (gateHandle.transform, false)
+				)
+			),
+			npcBehavior.NPCBehavior_DoGesture (GESTURE_CODE.GRAB_FRONT),
+			new LeafInvoke (() => gate.GetComponent <Animation> ().Play ("gate open")),
+			new LeafInvoke (() => gateOpened = true)
+		);
+	}
 
-    }
+	protected Node CloseGate () {
+		return new Sequence (
+			new LeafAssert (() => Vector3.Distance (gate.transform.position, guest.transform.position) > 22.0f),
+			new LeafAssert (() => gateOpened),
+			npcBehavior.NPCBehavior_Stop (),
+			new Selector (
+				new Sequence (
+					new LeafAssert (() => Vector3.Distance (gate.transform.position, gameObject.transform.position) > 40.0f),
+					npcBehavior.NPCBehavior_GoTo (gateHandle.transform, true)
+				),
+				new Sequence (
+					new LeafAssert (() => Vector3.Distance (gate.transform.position, gameObject.transform.position) <= 40.0f),
+					npcBehavior.NPCBehavior_GoTo (gateHandle.transform, false)
+				)
+			),
+			npcBehavior.NPCBehavior_DoGesture (GESTURE_CODE.GRAB_FRONT),
+			new LeafInvoke (() => gate.GetComponent <Animation> ().Play ("gate close")),
+			new LeafInvoke (() => gateOpened = false)
+		);
+	}
 
-    protected Node trigger(Func<bool> a)
-    {
-        return new LeafAssert(a);
-    }
+	protected Node StandGuard () {
+		return new Sequence (
+			npcBehavior.NPCBehavior_GoTo (guardPosition.transform, false),
+			npcBehavior.NPCBehavior_OrientTowards (gate.transform.position)
+		);
+	}
 
-    protected Node Goto(GameObject p, Vector3 place)
-    {
-        return new LeafInvoke(()=>p.GetComponent<NPCController>().GoTo(place));
-    }
+	protected Node ClickMove () {
+		return new Sequence (
+			new LeafAssert (() => selectionIndicator.activeInHierarchy),
+			new LeafAssert (() => Input.GetMouseButtonDown (1)),
+			new LeafAssert (() => Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 1000.0f)),
+			npcBehavior.NPCBehavior_GoTo (Val.V(() => hit.point), true)
+		);
+	}
 
-    /*protected Node wander(GameObject p)
-    {
-        int milli = System.DateTime.Now.Millisecond;
-        UnityEngine.Random.InitState(milli);
-        float rand = UnityEngine.Random.value * 20 - 10;
-        float rand2 = UnityEngine.Random.value * 20 - 10;
-        return new Sequence(new LeafTrace(milli.ToString()), new LeafTrace(rand.ToString()), new LeafTrace(rand2.ToString()), Goto(p, p.transform.position + new Vector3(rand, 0, rand2)), new LeafWait(10000));
-        return new Sequence(Goto(p, p.transform.position + new Vector3(1, 0, 1)), new LeafWait(5000));
-    }*/
-
-    protected Node BuildTreeRoot()
-    {
-        return new DecoratorLoop(new DecoratorForceStatus(RunStatus.Success, new Sequence(
-                //Goto(guest, gate.transform.position + new Vector3(0, 0, 2)),
-                new DecoratorLoop(new DecoratorForceStatus(RunStatus.Success, new Sequence(
-                    trigger(() => Vector3.Distance(guest.transform.position, gate.transform.position) < 4),
-                    Goto(guard, gate.transform.position + new Vector3(0, 0, -2)),
-                    new DecoratorLoop(new DecoratorForceStatus(RunStatus.Success, new Sequence(
-                        trigger(() => guard.GetComponent<NPCBody>().IsAtTargetLocation(gate.transform.position + new Vector3(0, 0, -2))),
-                        new LeafWait(1000),
-                        // GATE OPENING ANIMATION
-                        new LeafInvoke(() => anim.Play("Grab_Front")),
-                        //Goto(guest, gate.transform.position + new Vector3(15, 0, -5)),
-                        new DecoratorLoop(new DecoratorForceStatus(RunStatus.Success, new Sequence(
-                            trigger(() => Vector3.Distance(guest.transform.position, gate.transform.position) > 10),
-                            new LeafWait(1000),
-                            // GATE CLOSING ANIMATION
-                            new LeafInvoke(() => anim.Play("Grab_Front")),
-                            Goto(guard, gate.transform.position + new Vector3(10, 0, -10))
-                        )))
-                    )))
-                )))
-        )));
-    }
+	protected Node BuildTreeRoot () {
+		return new DecoratorLoop (
+			new DecoratorForceStatus (
+				RunStatus.Success,
+				new Selector (
+					ClickMove (),
+					OpenGate (),
+					CloseGate (),
+					StandGuard ()
+				)
+			)
+		);
+	}
 }
 /*
 Need to:
---
+++ Assets/Indivisuals/Scripts/SelectionController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using TreeSharpPlus;
+using NPC;
+
+public class SelectionController : MonoBehaviour {
+
+	private NPCBehavior aiScript;
+
+	void Awake() {
+		
+		aiScript = GetComponentInParent <NPCBehavior> ();
+	}
+
+	void Update() {
+		
+		if (Input.GetMouseButtonDown (1)) {
+			
+			RaycastHit hit;
+
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 1000.0f)) {
+				aiScript.NPCBehavior_Stop ();
+				aiScript.NPCBehavior_GoTo (Val.V(() => hit.point), true);
+			}
+		}
+			
+	}
+}
+
